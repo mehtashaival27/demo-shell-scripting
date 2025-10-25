a991aad0cebc74ba59d70de4249d21861c47d066
mehtshaival27 <mehtshaival27@gmail.com>
2025-10-25 15:32:58 +0000

    scripting files added

---
+++ functions.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+
+<<help
+This is a Shell Script to create user
+help
+
+echo" Hello World"
---
+++ loop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+<< info
+This scripting file is for Loops
+info
+
+for (( num=1 ; num<=10 ; num++ ))
+do
+	echo "$num"
+done
